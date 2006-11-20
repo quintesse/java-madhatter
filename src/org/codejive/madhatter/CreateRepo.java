9324598e30acce6b2c197a501935ea1538c4a795
tako <tako@esperanza.palacio-cristal.loc>
2006-11-20 23:57:25 +0100

    Changed license to GPL + classpath exception. Added license where missing.

--- src/org/codejive/madhatter/CreateRepo.java
+++ src/org/codejive/madhatter/CreateRepo.java
@@ -1,8 +1,46 @@
+/*
+ * [madhatter] Codejive CMS package
+ * 
+ * Copyright (C) 2006 Tako Schotanus
+ * 
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ * 
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library.  Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under
+ * terms of your choice, provided that you also meet, for each linked
+ * independent module, the terms and conditions of the license of that
+ * module.  An independent module is a module which is not derived from
+ * or based on this library.  If you modify this library, you may extend
+ * this exception to your version of the library, but you are not
+ * obligated to do so.  If you do not wish to do so, delete this
+ * exception statement from your version.
+ * 
+ * Created on October 6, 2006
+ */
 package org.codejive.madhatter;
 
 import java.io.IOException;
 
-import javax.jcr.Repository;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
@@ -34,11 +72,12 @@ public class CreateRepo extends javax.servlet.http.HttpServlet implements javax.
      * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request,
      *      HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request,
+    @Override
+	protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
         try {
             InitialContext context = new InitialContext();
-            Repository repository = (Repository) context.lookup("java:comp/env/jcr/repository");
+            context.lookup("java:comp/env/jcr/repository");
         } catch (NamingException e) {
             throw new ServletException("Could not create initial repository", e);
         }
@@ -50,7 +89,8 @@ public class CreateRepo extends javax.servlet.http.HttpServlet implements javax.
      * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request,
      *      HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
     }
 }
